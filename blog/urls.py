a1c3ac196d2c10efa79cc4d2d47c2bdfe1288b2a
ronald <rmaravanyika@gmail.com>
2016-04-24 13:18:41 +0200

    added form.py..css .photos..home.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.index, name='index'),
+    url(r'^post_list/', views.post_list, name='post_list'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^gallery/', views.gallery, name='gallery'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
+def index(request):
+    return render(request, 'blog/home.html')
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def contact(request):
+    return render(request, 'blog/basic.html',{'content':['If you would like to contact me, please email me.','rmaravanyika@gmail.com']})
+
+def gallery(request):
+    return render(request, 'blog/photos.html')
+
+def post_new(request):
+	form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
