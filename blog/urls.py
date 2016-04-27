d57698a71c56f1840b73e6b0eee2b2fd64a29df0
ronald <rmaravanyika@gmail.com>
2016-04-27 15:53:08 +0200

    added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^blog_index/', views.blog_index, name='blog_index'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^contact/', views.contact, name='contact'),
     url(r'^gallery/', views.gallery, name='gallery'),
 ]
