2e5eb77fe1cb6e4a76f3e093f47e5524344ba8f8
ronald <rmaravanyika@gmail.com>
2016-04-27 14:42:15 +0200

    added view and template for detailed blog post as well as css for si

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^blog_index/', views.blog_index, name='blog_index'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^contact/', views.contact, name='contact'),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -9,6 +9,10 @@ def blog_index(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
 
 def contact(request):
     return render(request, 'blog/basic.html',{'content':['If you would like to contact me, please email me.','rmaravanyika@gmail.com']})
