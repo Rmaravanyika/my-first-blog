c8589a2e6ffe20236c3bd5d8daf07f7d104ef9ab
ronald <rmaravanyika@gmail.com>
2016-04-27 18:14:59 +0200

    security and also puting the publish button

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url(r'^blog_index/', views.blog_index, name='blog_index'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
     url(r'^contact/', views.contact, name='contact'),
     url(r'^gallery/', views.gallery, name='gallery'),
 ]
